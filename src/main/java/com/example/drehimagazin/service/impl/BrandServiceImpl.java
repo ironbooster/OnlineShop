b35703d8598b4e2a8a10c81aead8ef0a41d2b06a
ironbooster <ironbooster014@gmail.com>
2022-02-13 04:13:07 +0200

    adding repository and entity v2

--- src/main/java/com/example/drehimagazin/service/impl/BrandServiceImpl.java
+++ src/main/java/com/example/drehimagazin/service/impl/BrandServiceImpl.java
@@ -1,16 +1,29 @@
 package com.example.drehimagazin.service.impl;
 
+import com.example.drehimagazin.repository.BrandRepository;
 import com.example.drehimagazin.service.BrandService;
+import com.example.drehimagazin.web.model.entities.BrandEntity;
 import com.example.drehimagazin.web.model.view.BrandViewModel;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class BrandServiceImpl implements BrandService {
 
+    private BrandRepository brandRepository;
+
+    public BrandServiceImpl(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
 
     @Override
     public List<BrandViewModel> getAllBrands() {
-        return null;
+        List<BrandViewModel> brandViewModels = new ArrayList<>();
+        List<BrandEntity> allBrands = brandRepository.findAll();
+        allBrands.forEach(me ->{
+            //TODO
+        });
+        return brandViewModels;
     }
 }
